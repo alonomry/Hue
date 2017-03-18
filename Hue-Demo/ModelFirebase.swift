5f791b1706c2f63454e9510acd3ca04bc3d06122
Omryno1 <Omryno1@gmail.com>
2017-03-18 19:30:01 +0200

    reArranging moderFireBase

--- Hue-Demo/ModelFirebase.swift
+++ Hue-Demo/ModelFirebase.swift
@@ -33,6 +33,78 @@ class ModelFirebase{
         return nil
     }
     
+    func getImageData(success : @escaping (Image, String) -> Void) {
+        FIRDatabase.database().reference().child("Posts").queryLimited(toFirst: 15).observe(.childAdded, with: { (snapshot) in
+            
+            let imageID = snapshot.key
+            if let dictionary = snapshot.value as? [String : Any]{
+                let feedImage = Image(json: dictionary)
+                success(feedImage, imageID)
+            }
+        })
+        
+    }
+    
+    func getProfileData(success : @escaping (Profile, UInt) -> Void){
+        FIRDatabase.database().reference().child("Users").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let users = snapshot.value as? [String : Any] {
+                for user in users {
+                    if let userProfile = user.value as? [String : Any]{
+                        let feedProfile = Profile(json: userProfile)
+                        
+                        //Sending the profile obj, num of childeren
+                        success(feedProfile, snapshot.childrenCount)
+                    }
+                }
+            }
+        })
+    }
+    
+    func saveImageToFireBase(image : UIImage, success : @escaping (String)->()){
+        
+        if let userUID = getUser()?.uid {
+            
+                    //Adding the image to firebase storage
+                    let metaData = FIRStorageMetadata()
+                    let imageName = NSUUID().uuidString
+                    let userUploadRef = FIRStorage.storage().reference().child(userUID).child("Uploads").child("\(imageName).jpeg")
+                    if let userData = UIImageJPEGRepresentation(image, 0.8){
+                        metaData.contentType = "image/jpeg"
+                        userUploadRef.put(userData, metadata: metaData, completion: { (metadata : FIRStorageMetadata?, error : Error?) in
+                            if error != nil{
+                                print(error!)
+                                return
+                            }
+            
+                            //Adding the image object to firbase database
+                            if let imageDownloadURL = metadata?.downloadURL()?.absoluteString{
+                                let uploadTime = Date()
+                                let upload = Image(imageUid : imageName ,url: imageDownloadURL, title: "", date: uploadTime , owner: userUID)
+                                let userProfileRef = FIRDatabase.database().reference().child("Posts").child(imageName)
+                                userProfileRef.setValue(upload.toAnyObject())
+            
+                            //Updating or Adding imageName to the user Posts array
+                                let userPostsRef = FIRDatabase.database().reference().child("Users").child(userUID).child("User_Posts")
+            
+                                userPostsRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                                    if (snapshot.hasChildren()){
+                                        if var userPosts = snapshot.value as? [String] {
+                                            userPosts.append(imageName)
+                                            userPostsRef.setValue(userPosts)
+                                        }
+                                    }
+                                    else {
+                                        userPostsRef.setValue([imageName])
+                                    }
+                                })
+                            }
+                            
+                        })
+                    }
+        }
+    }
+    
     func getDataBaseReference()->FIRDatabaseReference{
         return FIRDatabase.database().reference()
     }
